165c60dd0a9ff62e319b3df92e485a4bf801a9bc
Corentin Herbert <corentin.herbert@ens-lyon.fr>
2016-11-22 13:18:16 +0100

    More flexible database access

--- data.py
+++ data.py
@@ -56,3 +56,44 @@ class FirstPassageData(Database):
                 self.__setitem__(key[:-1],data)
 
         return data[:n]
+
+    def getsamples(self,eps,M,**kwargs):
+        """ A more flexible way to access the database: only epsilon and M are required arguments.
+        Initial conditions and timestep are optional arguments.
+        By default, we select the earliest available initial time, an initial condition on the attractor, and the smallest timestep available.
+        Note that the order matters: we select the timestep after the initial time. """
+        def parseopt(data,optlabel,optdict):
+            return (data == optdict.get(optlabel) if optlabel in optdict else True)
+        epslist,t0list,x0list,dtlist,Mlist = np.array(zip(*self.keys()))
+        mask = (epslist==eps)*(Mlist == M)*parseopt(t0list,'t0',kwargs)*parseopt(x0list,'x0',kwargs)*parseopt(dtlist,'dt',kwargs)
+
+        # If t0 is not given, take the largest one in the database
+        if 't0' in kwargs:
+            t0 = kwargs.get('t0')
+        else:
+            if len(t0list[mask]) >0:
+                t0 = np.min(t0list[mask])
+            else:
+                return np.array([])
+        # If x0 is not given, assume -sqrt(-t0) (i.e. on the attractor)
+        x0 = kwargs.get('x0',-np.sqrt(np.abs(t0)))
+        # If dt is not given, take the smallest value in the database
+        if 'dt' in kwargs:
+            dt = kwargs.get('dt')
+        else:
+            if len(dtlist[mask*(t0list == t0)]) > 0:
+                dt = np.min(dtlist[mask*(t0list == t0)])
+            else:
+                return np.array([])
+
+        # If n is not given, read all the samples we have
+        if 'n' in kwargs:
+            return self.__getitem__((eps,t0,x0,dt,M,kwargs.get('n')))
+        else:
+            return self.__getitem__((eps,t0,x0,dt,M))
+    
+    def show_eps(self):
+        """ Return the list of noise amplitudes stored in the database """
+        return set(zip(*self.keys())[0])
+    
+    
