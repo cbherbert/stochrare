6e04e36e7150f7d1d021f3b048484d27820820d5
Corentin Herbert <corentin.herbert@ens-lyon.fr>
2017-01-12 11:12:30 +0100

    Correct FP integration

--- stochtrans1D.py
+++ stochtrans1D.py
@@ -13,14 +13,14 @@ class StochModel(object):
         It corresponds to the family of 1D SDEs dx_t = F(x_t,t)dt + sqrt(2*D0)dW_t """
 
     default_dt = 0.1
-    
+
     def __init__(self,vecfield,Damp):
         """ vecfield is a function of two variables (x,t) and Damp the amplitude of the diffusion term (noise) """
-        self.F  = vecfield 
+        self.F  = vecfield
         self.D0 = Damp
 
     def potential(self,X,t):
-        """ Integrate the vector field to obtain the value of the underlying potential at the input points. 
+        """ Integrate the vector field to obtain the value of the underlying potential at the input points.
         Caveat: This works only because we restrict ourselves to 1D models. """
         fun = interp1d(X,-self.F(X,t))
         return np.array([integrate.quad(fun,0.0,x)[0] for x in X])
@@ -29,11 +29,11 @@ class StochModel(object):
         """ Return F(x_t,t)dt + sqrt(2*D0)dW_t """
         dt = kwargs.get('dt',self.default_dt)
         return self.F(x,t) * dt + np.sqrt(2*self.D0*dt)*np.random.normal(0.0,1.0)
-        
+
     def time_reversal(self):
         """ Apply time reversal and return the new model """
         return StochModel_T(lambda x,t: -self.F(x,-t),self.D0)
-        
+
     def trajectory(self,x0,t0,**kwargs):
         """ Integrate a trajectory with given initial condition (t0,x0) """
         x      = [x0]
@@ -41,23 +41,23 @@ class StochModel(object):
         time   = kwargs.get('T',10)   # Total integration time
         if dt < 0: time=-time
         tarray = np.linspace(t0,t0+time,num=time/dt+1)
-        for t in tarray[1:]:    
+        for t in tarray[1:]:
             x += [ x[-1] + self.increment(x[-1],t,dt=dt)]
         x = np.array(x)
-        if kwargs.get('finite',False):            
+        if kwargs.get('finite',False):
             tarray = tarray[np.isfinite(x)]
             x = x[np.isfinite(x)]
         return tarray,x
 
     @classmethod
-    def trajectoryplot(cls,*args,**kwargs):        
+    def trajectoryplot(cls,*args,**kwargs):
         """ Plot previously computed trajectories """
         fig = plt.figure()
         ax  = plt.axes()
         lines = []
         for t,x in args:
-            lines += ax.plot(t,x)        
-        
+            lines += ax.plot(t,x)
+
         ax.grid()
         ax.set_ylim(kwargs.get('ylim',ax.get_ylim()))
         ax.set_xlim(kwargs.get('xlim',ax.get_xlim()))
@@ -70,9 +70,9 @@ class StochModel(object):
         labels = kwargs.get('labels',[])
         if labels != []:
             plt.legend(lines,labels,bbox_to_anchor=(1.05, 1),loc=2, borderaxespad=0.)
-        
+
         cls._trajectoryplot_decorate(*args,axis=ax,**kwargs)
-        plt.show()        
+        plt.show()
 
     @classmethod
     def _trajectoryplot_decorate(cls,*args,**kwargs):
@@ -82,7 +82,7 @@ class StochModel(object):
         """ Compute the last time with finite values, for one realization"""
         t,x = self.trajectory(x0,t0,**kwargs)
         return t[np.isfinite(x)][-1]
-    
+
     def _fpeq(self,P,X,t):
         """ Right hand side of the Fokker-Planck equation associated to the stochastic process """
         return -X.grad(self.F(X.grid,t)*P) + self.D0*X.laplacian(P)
@@ -97,8 +97,8 @@ class StochModel(object):
 
     def _fpadjmat(self,X,t):
         """ Sparse matrix representation of the adjoint of the FP operator """
-        return sps.dia_matrix((self.F(X.grid,t)[1:-1],np.array([0])),shape=(X.N-2,X.N-2))*X.grad_mat() + self.D0*X.lapl_mat()        
-    
+        return sps.dia_matrix((self.F(X.grid,t)[1:-1],np.array([0])),shape=(X.N-2,X.N-2))*X.grad_mat() + self.D0*X.lapl_mat()
+
     def _fpbc(self,fdgrid,bc=('absorbing','absorbing'),**kwargs):
         """ Build the boundary conditions for the Fokker-Planck equation and return it.
         This is useful when at least one of the sides is a reflecting wall. """
@@ -113,11 +113,11 @@ class StochModel(object):
             return bc
 
     def _fpthsol(self,X,t,**kwargs):
-        """ Analytic solution of the Fokker-Planck equation, when it is known. 
+        """ Analytic solution of the Fokker-Planck equation, when it is known.
         In general this is an empty method but subclasses corresponding to stochastic processes
         for which theoretical results exists should override it."""
         pass
-    
+
     def fpintegrate(self,t0,T,**kwargs):
         """
         Numerical integration of the associated Fokker-Planck equation, or its adjoint.
@@ -141,9 +141,13 @@ class StochModel(object):
         P0     = kwargs.pop('P0','gauss')
         if P0 is 'gauss':
             P0 = np.exp(-0.5*((fdgrid.grid-kwargs.get('P0center',0.0))/kwargs.get('P0std',1.0))**2)/(np.sqrt(2*np.pi)*kwargs.get('P0std',1.0))
+            P0 /= integrate.trapz(P0,fdgrid.grid)
         if P0 is 'dirac':
             P0 = np.zeros_like(fdgrid.grid)
             np.put(P0,len(fdgrid.grid[fdgrid.grid<kwargs.get('P0center',0.0)]),1.0)
+            P0 /= integrate.trapz(P0,fdgrid.grid)
+        if P0 is 'uniform':
+            P0 = np.ones_like(fdgrid.grid)
             P0 /= integrate.trapz(P0,fdgrid.grid)
         # Numerical integration:
         if T>0:
@@ -155,7 +159,18 @@ class StochModel(object):
                 return edpy.EDPSolver().edp_int(fpfun,fdgrid,P0,t0,T,dt,bc)
         else:
             return t0,fdgrid.grid,P0
-    
+
+    def pdfgen(self,*args,**kwargs):
+        """ Generate the pdf solution of the FP equation at various times """
+        t0  = kwargs.pop('t0',args[0])
+        fun = kwargs.pop('integ',self.fpintegrate)
+        for t in args:
+            t,X,P = fun(t0,t-t0,**kwargs)
+            t0 = t
+            kwargs['P0'] = P
+            yield t, X, P
+
+
     def pdfplot(self,*args,**kwargs):
         """ Plot the pdf P(x,t) at various times """
         fig = plt.figure()
@@ -163,7 +178,7 @@ class StochModel(object):
         t0  = kwargs.pop('t0',args[0])
         fun = kwargs.pop('integ',self.fpintegrate)
         if kwargs.get('potential',False):
-            ax2 = ax.twinx()            
+            ax2 = ax.twinx()
             ax2.set_ylabel('$V(x,t)$')
         for t in args:
             t,X,P = fun(t0,t-t0,**kwargs)
@@ -179,8 +194,8 @@ class StochModel(object):
         ax.grid()
         ax.set_xlabel('$x$')
         ax.set_ylabel(kwargs.get('ylabel','$P(x,t)$'))
-        plt.title('$\epsilon='+str(self.D0)+'$')        
-        ax.legend()
+        plt.title('$\epsilon='+str(self.D0)+'$')
+        ax.legend(**(kwargs.get('legend_args',{})))
         plt.show()
 
     # First passage time problems:
@@ -246,14 +261,17 @@ class StochModel(object):
         t0 = kwargs.pop('t0',0.0)
         if 'P0' in kwargs: del kwargs['P0']
         if 'P0center' in kwargs: del kwargs['P0center']
-        G = []
-        t,X,P = self.fpintegrate(t0,0.0,P0='dirac',P0center=x0,**kwargs)       
-        for t in args:
-            t,X,P = self.fpintegrate(t0,t-t0,P0=P,**kwargs)
+        if 'bc' not in kwargs: kwargs['bc'] = ('reflecting','absorbing')
+        bnds = (kwargs.pop('bounds',(-10.0,0.0))[0],A)
+        time = np.sort([t0]+list(args))
+        time = time[time >= t0]
+        G = [1.0 if x0 < A else 0.0]
+        t,X,P = self.fpintegrate(t0,0.0,P0='dirac',P0center=x0,bounds=bnds,**kwargs)
+        for t in time[1:]:
+            t,X,P = self.fpintegrate(t0,t-t0,P0=P,bounds=bnds,**kwargs)
             G += [integrate.trapz(P[X<A],X[X<A])]
             t0 = t
         G = np.array(G)
-        time = np.array(args)
         output = {'cdf': (time,1.0-G), 'G': (time,G), 'pdf': (time[1:-1],-edpy.CenteredFD(time).grad(G)), 'lambda': (time[1:-1],-edpy.CenteredFD(time).grad(np.log(G)))}
         return output.get(kwargs.get('out','G'))
 
@@ -280,18 +298,18 @@ class Wiener(StochModel):
         return np.zeros_like(X)
 
     def _fpthsol(self,X,t,**kwargs):
-        """ Analytic solution of the heat equation. 
+        """ Analytic solution of the heat equation.
         This should depend on the boundary conditions.
-        Right now, we do as if we were solving on the real axis."""        
+        Right now, we do as if we were solving on the real axis."""
         return np.exp(-X**2.0/(4.0*self.D0*t))/np.sqrt(4.0*np.pi*self.D0*t)
 
-    
+
 class OrnsteinUhlenbeck(StochModel):
     """ The Ornstein-Uhlenbeck model """
     def __init__(self,mu,theta,D):
         super(self.__class__,self).__init__(lambda x,t: theta*(mu-x),D)
-        
-    
+
+
 class DrivenDoubleWell(StochModel):
     """ Double well potential model, possibly including periodic forcing and noise """
 
@@ -306,14 +324,14 @@ class DrivenDoubleWell(StochModel):
         """ Return the value of the potential at the input points """
         Y = X**2
         return Y*(Y-2.0)/4.0-X*self.Famp*np.sin(self.Om*t)
-        
+
     def phaseportrait(self,a,b,ntraj,niter,dt):
-        """ Compute and plot the trajectories for an ensemble of initial conditions """        
+        """ Compute and plot the trajectories for an ensemble of initial conditions """
         for x0 in np.linspace(a,b,num=ntraj):
-            plt.plot(*self.trajectory(x0,0,T=niter*dt,dt=dt))        
+            plt.plot(*self.trajectory(x0,0,T=niter*dt,dt=dt))
         plt.xlabel('$t$')
         plt.ylabel('$x(t)$')
-        plt.show()        
+        plt.show()
 
     @classmethod
     def _trajectoryplot_decorate(cls,*args,**kwargs):
@@ -324,16 +342,16 @@ class DrivenDoubleWell(StochModel):
         ax.axhline(y=-1.0,linewidth=1,color='black')
         # tmin = kwargs.get('tmin',np.min(zip(*args)[0]))
         # tmax = kwargs.get('tmax',np.max(zip(*args)[0]))
-        # ax.set_xlim([tmin,tmax])        
+        # ax.set_xlim([tmin,tmax])
 
 
     def running_mean(x, N):
         cumsum = np.cumsum(np.insert(x, 0, 0))
-        return (cumsum[N:] - cumsum[:-N]) / N 
-        
+        return (cumsum[N:] - cumsum[:-N]) / N
+
     def transitionrate(self,x,**kwargs):
         """ Count the number of transitions from one attractor to the other for a given trajectory.
-            Without smoothing (avg=1), that should be the number of items in the generator levelscrossing(x,0) when starting with the right transition, 
+            Without smoothing (avg=1), that should be the number of items in the generator levelscrossing(x,0) when starting with the right transition,
             or that number +1 if we use the wrong isign in levelscrossing."""
         window = kwargs.get('avg',1)
         y = self.running_mean(x,window) if window > 1 else x
@@ -343,13 +361,13 @@ class DrivenDoubleWell(StochModel):
         """ Maps the stochastic process x(t) onto a stochastic process {t_i} where the 't_i's correspond to crossing levels +- c """
         sign = kwargs.get('isign',1) # By default we start by detecting the transition below the -c threshold
         if sign == 0: sign=1
-        if not abs(sign) == 1: sign /= abs(sign)         
+        if not abs(sign) == 1: sign /= abs(sign)
         for i in xrange(len(x)-1):
             if (c+sign*x[i]) > 0 and (c+sign*x[i+1]) < 0:
                 sign *= -1
                 yield i
 
-    def residencetimes(self,x,c):        
+    def residencetimes(self,x,c):
         transtimes = np.array([t for t in self.levelscrossing(x,c)])
         return transtimes[1:]-transtimes[:-1]
 
@@ -374,7 +392,7 @@ class DrivenDoubleWell(StochModel):
         """ Numerical integration of the associated Fokker-Planck equation.
         We only modify the default values for a few parameters, to adjust to the specificities of the model:
         - Default boundaries do not need to be so wide (potential is very steep)
-        - Default initial condition is (a Gaussian) centered on one of the wells (-1) with smaller standard deviation 
+        - Default initial condition is (a Gaussian) centered on one of the wells (-1) with smaller standard deviation
         - Default boundary conditions are reflecting on both sides """
         return super(DrivenDoubleWell,self).fpintegrate(t0,T,bounds=kwargs.pop('bounds',(-3.0,3.0)),P0center=kwargs.pop('P0center',-1.0),P0std=kwargs.pop('P0std',0.1),bc=kwargs.pop('bc',('reflecting','reflecting')),**kwargs)
 
@@ -392,7 +410,7 @@ class DrivenDoubleWell(StochModel):
         G0[fdgrid.grid >= A] = 0.0
         # Call integration routine with adjoint FP equation:
         return super(DrivenDoubleWell,self).fpintegrate(t0,T,bounds=(B,A),P0=kwargs.pop('P0',G0),bc=kwargs.pop('bc',edpy.BoundaryCondition(lambda Y,X,t: [Y[1],0])),adjoint=True,**kwargs)
-    
+
     def firstpassagetime_cdf(self,x0,A,*args,**kwargs):
         """ Computes the CDF of the first passage time, Prob_{x0,t0}[\tau_A<t], either by solving the Fokker-Planck equation, its adjoint, or by using the theoretical solution. """
         src = kwargs.pop('src','FP')
@@ -470,14 +488,14 @@ class StochSaddleNode(StochModel):
     We use the normal form of the saddle-node bifurcation with a time dependent parameter, and add a stochastic term. """
 
     default_dt = 0.01
-    
+
     def __init__(self,Damp):
         super(StochSaddleNode,self).__init__(lambda x,t: x**2+t,Damp)
 
     def potential(self,X,t):
         """ Return the value of the potential at the input points """
         return -X**3/3.0-t*X
-        
+
     @classmethod
     def _trajectoryplot_decorate(cls,*args,**kwargs):
         """ Plot the fixed point trajectories """
@@ -489,9 +507,9 @@ class StochSaddleNode(StochModel):
         plt.plot(time,np.sqrt(np.abs(time)),linestyle='dashed',color='black')
 
     def trajectory(self,x0,t0,**kwargs):
-        """ This is a wrapper to the compiled saddlenode_trajectory function """        
+        """ This is a wrapper to the compiled saddlenode_trajectory function """
         time = kwargs.get('T',10)   # Total integration time
-        dt   = kwargs.get('dt',self.default_dt) # Time step                            
+        dt   = kwargs.get('dt',self.default_dt) # Time step
         if dt == 'adapt':
             t,x = saddlenode_trajectory_adapt(x0,self.D0,t0,time,self.default_dt)
         else:
@@ -499,11 +517,11 @@ class StochSaddleNode(StochModel):
             if dt < 0: time = -time
             t = np.linspace(t0,t0+time,num=time/dt+1)
             x = saddlenode_trajectory(x0,dt,self.D0,t[1:])
-        if kwargs.get('finite',False):            
+        if kwargs.get('finite',False):
             t = t[np.isfinite(x)]
             x = x[np.isfinite(x)]
         return t,x
-    
+
     def escapetime_sample(self,x0,t0,A,**kwargs):
         """ This is a wrapper to the compiled vec_escape_time function """
         # Input parameters and options:
@@ -540,10 +558,10 @@ class StochSaddleNode(StochModel):
 
     def fpintegrate(self,t0,T,**kwargs):
         """ Numerical integration of the associated Fokker-Planck equation.
-        We only modify the default values for a few parameters, to adjust to the specificities of the model:        
+        We only modify the default values for a few parameters, to adjust to the specificities of the model:
         - Default initial condition is (a Gaussian) centered on the attractor
         - Default boundary conditions are reflecting on the left and absorbing on the right (potential does not go up on that side)
-        Default boundaries, etc, are fine """        
+        Default boundaries, etc, are fine """
         return super(self.__class__,self).fpintegrate(t0,T,P0center=kwargs.pop('P0center',-np.sqrt(np.abs(t0))),bc=kwargs.pop('bc',('reflecting','absorbing')),**kwargs)
 
     def pdfplot(self,*args,**kwargs):
@@ -559,7 +577,7 @@ class StochSaddleNode(StochModel):
             t = t[t<0]
             G = np.exp(-(3./4.)**(2./3.)*(self.D0)**(5./3.)*gammaincc(5./3.,4.*(-t)**1.5/(3.*self.D0))*gamma(5./3.)/(6.*np.pi))
             Lambda = (-t)**(1.5)*np.exp(-4.*(-t)**1.5/(3.*self.D0))/(3*np.pi)
-            P = Lambda*G            
+            P = Lambda*G
             return t,{'cdf': 1.0-G, 'G': G, 'pdf': P, 'lambda': Lambda}.get(kwargs.get('out','G'))
         else:
             return super(self.__class__,self).firstpassagetime_cdf(x0,A,*args,**kwargs)
@@ -569,14 +587,14 @@ class StochSaddleNode(StochModel):
         def fun(Y,t):
             return (Y[0]**2+2.*Y[1]+t,-2.*Y[0]*Y[1])
         return integrate.odeint(fun,(x0,p0),args,**kwargs)
-        
+
 class DynSaddleNode(StochSaddleNode):
     """ This is just the deterministic version of the dynamical saddle-node bifurcation dx/dt = x^2+t, for which we have an analytic solution """
 
     tstar = -ai_zeros(1)[0][0]
-    
+
     def __init__(self):
-        super(self.__class__,self).__init__(0)        
+        super(self.__class__,self).__init__(0)
 
     @classmethod
     def formula(cls,t,A0,B0):
@@ -586,16 +604,16 @@ class DynSaddleNode(StochSaddleNode):
     @classmethod
     def coefficients(cls,x0,t0):
         ai,aip,bi,bip = airy(-t0)
-        alpha, beta = (x0*ai-aip,x0*bi-bip)        
+        alpha, beta = (x0*ai-aip,x0*bi-bip)
         if alpha == 0:
             A0, B0 = (1,0)
         else:
             A0, B0 = (-beta/alpha,1)
         return A0,B0
-            
+
     @classmethod
     def trajectory(cls,x0,t0,**kwargs):
-        """ Analytic solution for trajectories """        
+        """ Analytic solution for trajectories """
         dt     = kwargs.get('dt',cls.default_dt) # Time step
         time   = kwargs.get('T',10)   # Total integration time
         if dt < 0: time=-time
@@ -606,8 +624,8 @@ class DynSaddleNode(StochSaddleNode):
     def firstpassagetime(cls,x0,t0,A,**kwargs):
         """ Compute first passage time using the analytic solution """
         return brentq(lambda t: cls.formula(t,*cls.coefficients(x0,t0))-A,t0,cls.tstar)
-        
-        
+
+
 ###
 #
 #  Compiled code using numba for better performance
@@ -660,10 +678,10 @@ def saddlenode_trajectory_adapt(x0,D0,t0,T,dtmax):
     """ Integrate a trajectory with given initial condition (t0,x0) and adaptive timestep """
     x = [x0]
     t = [t0]
-    while t[-1] < t0+T:    
+    while t[-1] < t0+T:
         dt = min(dtmax,0.1/np.sqrt(np.abs(t[-1])))
         x += [ x[-1] + (x[-1]**2+t[-1]) * dt + np.sqrt(2*D0*dt)*np.random.normal(0.0,1.0)]
-        t += [ t[-1] + dt ]        
+        t += [ t[-1] + dt ]
     return np.array((t,x))
 
 
@@ -673,4 +691,3 @@ class StochModel_T(StochModel):
     def trajectory(self,x0,t0,**kwargs):
         t,x = super(self.__class__,self).trajectory(x0,t0,**kwargs)
         return 2*t[0]-t,x
-
