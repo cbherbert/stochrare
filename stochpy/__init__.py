3c9b67032b488a5437efc4adcbdc3a1d137d858a
Corentin Herbert <corentin.herbert@ens-lyon.fr>
2018-07-02 15:24:54 +0200

    Add basic time series analysis module

--- stochpy/__init__.py
+++ stochpy/__init__.py
@@ -4,6 +4,6 @@ stochpy is a package to simulate simple stochastic processes
 See README.md for more information.
 """
 
-from . import data, edpy, dynamics
+from . import data, edpy, dynamics, timeseries
 
-__all__ = ['data', 'edpy', 'dynamics']
+__all__ = ['data', 'edpy', 'dynamics', 'timeseries']
