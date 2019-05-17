1952b82e13ae92999ec20a1870b199568c2478c5
Corentin Herbert <corentin.herbert@ens-lyon.fr>
2019-05-17 16:00:26 +0200

    Create 1D PDF plotting routine

--- stochpy/io/plot.py
+++ stochpy/io/plot.py
@@ -10,6 +10,54 @@ This module contains several functions for making quick plots.
 """
 import matplotlib.pyplot as plt
 
+def pdf_plot1d(*args, **kwargs):
+    """
+    Plot PDFs.
+
+    Parameters
+    ----------
+    *args : variable length argument list
+        PDFs: tuple (X, P) or (X, P, kwargs_dict)
+
+    Keyword Arguments
+    -----------------
+    potential : ndarray 2-tuple
+        X, V where V is the value of the potential at the sample points X.
+        Default (None, None).
+    fig : matplotlig.figure.Figure
+        Figure object to use for the plot. Create one if not provided.
+    ax : matplotlig.axes.Axes
+        Axes object to use for the plot. Create one if not provided.
+    **kwargs :
+        Other keyword arguments forwarded to matplotlib.pyplot.axes.
+
+    Returns
+    -------
+    fig, ax: matplotlib.figure.Figure, matplotlib.axes.Axes
+        The figure.
+    """
+    X, V = kwargs.pop('potential', (None, None))
+    if 'fig' in kwargs:
+        fig = kwargs.pop('fig')
+    else:
+        fig = plt.figure()
+    if 'ax' in kwargs:
+        ax = kwargs.pop('ax')
+    else:
+        ax = plt.axes(**kwargs)
+    for pdf in args:
+        ax.plot(pdf[0], pdf[1], **(pdf[2] if len(pdf) > 2 else {}))
+    if V is not None:
+        ax2 = ax.twinx()
+        ax2.set_ylabel('$V(x,t)$')
+        ax2.plot(X, V, linestyle='dashed')
+    ax.grid()
+    ax.set_xlabel('$x$')
+    ax.set_ylabel(kwargs.get('ylabel', '$P(x,t)$'))
+    #ax.legend(**(kwargs.get('legend_args', {})))
+    ax.legend()
+    return fig, ax
+
 def returntime_plot(*args):
     """
     Make return time plot: amplitude a as a function of the return time r(a)
