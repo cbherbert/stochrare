4540b89f104a7ccd3a5a32a8bc674d233ca0a61a
Corentin Herbert <corentin.herbert@ens-lyon.fr>
2019-07-03 10:42:35 +0200

    Automatically import instanton module

--- stochpy/__init__.py
+++ stochpy/__init__.py
@@ -4,6 +4,6 @@ stochpy is a package to simulate simple stochastic processes
 See README.md for more information.
 """
 
-from . import dynamics, edpy, fokkerplanck, firstpassage, timeseries, rare, io, utils
+from . import dynamics, edpy, fokkerplanck, firstpassage, timeseries, rare, instanton, io, utils
 
-__all__ = ['dynamics', 'edpy', 'fokkerplanck', 'firstpassage', 'timeseries', 'rare', 'io', 'utils']
+__all__ = ['dynamics', 'edpy', 'fokkerplanck', 'firstpassage', 'timeseries', 'rare', 'instanton', 'io', 'utils']
