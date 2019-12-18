a16092a85274273546333e4800e7f9779f0c7835
Corentin Herbert <corentin.herbert@ens-lyon.fr>
2019-12-18 17:25:59 +0100

    Fix indentation bug

--- stochrare/edpy.py
+++ stochrare/edpy.py
@@ -98,7 +98,7 @@ class EDPSolver:
         # time integration
         t = t0
         while (t+dt <= t0+T):
-        P = np.copy(P0)
+            P = np.copy(P0)
             P[1:-1] += solfun(P, solgrid, t)*dt # Advancing in the bulk
             bc.apply(P, solgrid.grid, t)        # Applying boundary conditions
             t += dt
