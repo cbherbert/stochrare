b577ae686e5fe9072c7f3e730e4ad3c224cbdd7e
Corentin Herbert <corentin.herbert@ens-lyon.fr>
2020-04-27 12:52:06 +0200

    Add tests and doc for timeseries module

--- stochrare/tests/test_timeseries.py
+++ stochrare/tests/test_timeseries.py
@@ -1,3 +1,4 @@
+
 """
 Unit tests for the timeseries module.
 """
@@ -5,7 +6,54 @@ import unittest
 import numpy as np
 import stochrare.timeseries as ts
 
-class TestRareEvents(unittest.TestCase):
+class TestTimeseries(unittest.TestCase):
+    def test_runningmean(self):
+        x = np.random.random(10)
+        np.testing.assert_allclose(x, ts.running_mean(x, 1))
+        x = np.arange(10)
+        np.testing.assert_allclose(x[:-1]+0.5, ts.running_mean(x, 2))
+        np.testing.assert_allclose(x[1:-1], ts.running_mean(x, 3))
+
+    def test_transitionrate(self):
+        size = 100
+        data = np.random.random(size)
+        indices = np.array([88, 17, 28, 47, 14,  6, 81, 25, 62,  2])
+        nbtrans = len(indices)
+        #indices = np.random.randint(0, size, size=nbtrans)
+        #Note: to pick random indices like the above, one would have to be carefull
+        # about identical or contiguous indices.
+        data[indices] = np.random.random(nbtrans)+1
+        np.testing.assert_allclose(ts.transitionrate(data, 1), 2*nbtrans/size)
+        # We count transitions going up or down so we have to multiply nbtrans by 2.
+
+    def test_levelscrossing(self):
+        size = 100
+        data = np.random.random(size)
+        indices_up = np.array([88, 17, 28, 47, 14,  6, 81, 25, 62,  2])
+        indices_dn = np.array([20, 97, 18,  4, 95, 28, 78, 69, 92, 88])
+        nbtrans = len(indices_up)
+        data[indices_up] = np.random.random(nbtrans)+1
+        data[indices_dn] = np.random.random(nbtrans)-2
+        self.assertEqual(list(ts.levelscrossing(data, 1, sign=1)),
+                         [3, 5, 17, 24, 27, 46, 68, 80, 87])
+        self.assertEqual(list(ts.levelscrossing(data, 1, sign=-1)),
+                         [1, 3, 5, 17, 24, 27, 46, 68, 80, 87])
+        self.assertEqual(list(ts.levelscrossing(data, 1, sign=0)),
+                         list(ts.levelscrossing(data, 1, sign=1)))
+        self.assertEqual(list(ts.levelscrossing(data, 1, sign=2)),
+                         list(ts.levelscrossing(data, 1, sign=1)))
+
+    def test_residencetimes(self):
+        size = 100
+        data = np.random.random(size)
+        indices_up = np.array([88, 17, 28, 47, 14,  6, 81, 25, 62,  2])
+        indices_dn = np.array([20, 97, 18,  4, 95, 28, 78, 69, 92, 88])
+        nbtrans = len(indices_up)
+        data[indices_up] = np.random.random(nbtrans)+1
+        data[indices_dn] = np.random.random(nbtrans)-2
+        np.testing.assert_allclose(ts.residencetimes(data, 1),
+                                   np.array([2, 12, 7, 3, 19, 22, 12, 7]))
+
     def test_blockmaximum(self):
         data = np.random.random(101)
         data[20] = 2.0
--- stochrare/timeseries.py
+++ stochrare/timeseries.py
@@ -7,35 +7,76 @@ import numpy as np
 
 def running_mean(x, N):
     """
-    Return the running mean of a time series
+    Return the running mean of a time series.
+
+    Parameters
+    ----------
+    x: ndarray (1D)
+        The trajectory.
+    N: int
+        The window size.
+
+    Returns
+    -------
+    xavg: ndarray (1D)
+        If x has length n, then xavg has length n-N+1.
     """
     cumsum = np.cumsum(np.insert(x, 0, 0))
     return (cumsum[N:] - cumsum[:-N]) / N
 
-def transitionrate(x, threshold, **kwargs):
+def transitionrate(x, threshold, window=1):
     """
     Count the number of times a given trajectory goes across a given threshold.
     A typical use case is to study transitions from one attractor to the other.
 
-    x: the time series (a numpy array)
-    threshold: the threshold (e.g. separating the two attractors); a float
-    avg (optional): averaging window for smoothing timeseries before computing transition rate
+    Parameters
+    ----------
+    x: ndarray (1D)
+        The time series
+    threshold: float
+        The threshold (e.g. separating the two attractors)
+    window (optional): int, default=1
+        Averaging window for smoothing timeseries before computing transition rate.
 
+    Returns
+    -------
+    lambda: float
+        The transition rate.
+
+    Notes
+    -----
     Without smoothing (avg=1), the result should coincide with the number of items in the generator
     levelscrossing(x,0) when starting with the right transition,
     or that number +1 if we use the wrong isign in levelscrossing.
     """
-    window = kwargs.get('avg', 1)
     y = running_mean(x, window) if window > 1 else x
     return float(((y[1:]-threshold)*(y[:-1]-threshold) < 0).sum())/len(y)
 
-def levelscrossing(x, threshold, **kwargs):
+def levelscrossing(x, threshold, sign=1):
     """
     Maps the stochastic process x(t) onto a stochastic process {t_i}
     where the 't_i's correspond to crossing levels +- c
+
+    Parameters
+    ----------
+    x: ndarray (1D)
+        The time series
+    threshold: float
+        The threshold (e.g. separating the two attractors)
+    sign (optional): int, default=1
+        The initial sign: detect transitions going up (sign=-1) or down (sign=1) first.
+
+    Yields
+    ------
+    index: int
+        The indices corresponding to the transitions.
+
+    Notes
+    -----
+    This function is useful for transition between two states corresponding to two symmetric
+    thresholds with opposite signs.
     """
     # By default we start by detecting the transition below the -c threshold
-    sign = kwargs.get('isign', 1)
     if sign == 0:
         sign = 1
     if not abs(sign) == 1:
@@ -46,7 +87,31 @@ def levelscrossing(x, threshold, **kwargs):
             yield i
 
 def residencetimes(x, threshold):
-    transtimes = np.array([t for t in levelscrossing(x, threshold)])
+    """
+    Return the time spent in each of the attractors defined by the threshold.
+
+    Parameters
+    ----------
+    x: ndarray (1D)
+        The time series
+    threshold: float
+        The threshold (e.g. separating the two attractors)
+
+    Returns
+    -------
+    residencetime: ndarray (1D)
+        The time (number of samples) spent in each region between two transitions.
+
+    Notes
+    -----
+    This function is useful for transition between two states corresponding to two symmetric
+    thresholds with opposite signs.
+
+    In the future the function should be modified to generate realizations of two random variables,
+    corresponding to the time spent in each of the two regions.
+    Mybe it should be done as a generator yielding a pair of residence times.
+    """
+    transtimes = np.fromiter(levelscrossing(x, threshold), int)
     return transtimes[1:]-transtimes[:-1]
 
 
@@ -64,17 +129,28 @@ def blockmaximum(traj, nblocks, mode='proba', modified=False, **kwargs):
     block. We sort the maxima in descending order and assign probability n/nblocks to the maximum
     of rank n: in the input timeseries, maximum n has been reached n times.
 
-    Arguments
-    - traj: the timeseries X_t
-    - nblocks: the number of blocks. It should be chosen so that each block is larger than the
-               correlation time of the timeseries, but we also want as many blocks as possible.
-    - mode: 'proba' (default) or 'returntime': determine whether to return probability
-            or return time of the event X_t > a.
-    - modified: bool (default False)
-                Use the modified version of the bock maximum estimator defined in
-                Lestang, Ragone, Brehier, Herbert and Bouchet, J. Stat. Mech. 2018
-    - time: ndarray
-            sampling times (default is just 0,1,2,...)
+    Parameters
+    ----------
+    traj: ndarray (1D)
+        The timeseries X_t
+    nblocks: int
+        The number of blocks. It should be chosen so that each block is larger than the
+        correlation time of the timeseries, but we also want as many blocks as possible.
+    mode: str
+        'proba' (default) or 'returntime': determine whether to return probability
+        or return time of the event X_t > a.
+    modified: bool (default False)
+        Use the modified version of the bock maximum estimator defined in
+        Lestang, Ragone, Brehier, Herbert and Bouchet, J. Stat. Mech. 2018
+    time: ndarray
+        sampling times (default is just 0,1,2,...)
+
+    Yields
+    ------
+    a, p: float, float
+        The amplitude and associated probability (mode 'proba')
+    a, r: float, float
+        The amplitude and associated return time (mode 'returntime')
     """
     time = kwargs.get('time', np.arange(len(traj)))
     trajlen = float(time[-1]-time[0])
